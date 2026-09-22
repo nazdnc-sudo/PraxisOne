0785ef7321b3025ad00e3b5d8e33f14519fa0bba
nazdnc-sudo <naz.dnc@icloud.com>
2026-09-22 20:53:43 +0200

    Delete PraxisOne_/PraxisOne directory

--- PraxisOne_/PraxisOne/Includes/footer.php
+++
@@ -1,8 +0,0 @@
-</div><!-- /container -->
-
-<footer class="footer">
-    <span>PraxisOne &copy; <?= date('Y') ?> &middot; Webbasiertes Patientenbetreuungssystem</span>
-</footer>
-
-</body>
-</html>
--- PraxisOne_/PraxisOne/register.php
+++
@@ -1,5 +0,0 @@
-<?php
-// register.php ist deaktiviert (Lastenheft §2.2: Patienten werden von Hand angelegt).
-// Direkte URL-Zugriffe werden zur Login-Seite weitergeleitet.
-header("Location: login.php");
-exit();
